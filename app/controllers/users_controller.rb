f3ba6363c14783e28a3b1a3c137430e9c619691b
abokado0615-cell <abokado0615@gmail.com>
2026-06-20 02:59:03 +0900

    fix: users_controllerを整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,8 @@
 class UsersController < ApplicationController
+  # 詳細表示の機能だけ残す
   def show
     @user = User.find(params[:id])
   end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      # 保存に成功したら、ユーザーの詳細画面（マイページ）に戻る
-      redirect_to user_path(@user), notice: "プロフィールを更新しました！"
-    else
-      # 保存に失敗したら、もう一度編集画面を表示する
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def user_params
-    # 許可する項目をすべてコンマで区切って一列に綺麗に並べました
-    params.require(:user).permit(:name, :email, :image, :profile, :occupation, :study_tags, :instagram_id, :x_id, :tiktok_id)
-  end
+  # プロフィール更新（update）はDevise側で制御するので、ここからは削除してOK
 end
